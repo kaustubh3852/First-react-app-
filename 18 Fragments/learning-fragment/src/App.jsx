3396a48983aefa9c07f4641c5ad58aa616842c58
Kaustubh Raj <manman3852@gmail.com>
2025-08-03 21:10:32 +0530

    Initial commit

---
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy Foods</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Green Vegitables</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Salad</li>
+        <li class="list-group-item">Milk</li>
+      </ul>
+</>  );
+}
+
+export default App;
